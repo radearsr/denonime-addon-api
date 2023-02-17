0ccad9856ba5c501a22b14ecf5bc38e8259d6cfa
radearsr <radea.surya1@gmail.com>
2023-02-18 05:55:24 +0700

    update to add root content and 404 where routes not list in app

--- server.js
+++ server.js
@@ -7,6 +7,28 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", require("./routes/api"));
 
+app.get("/", (req, res) => {
+  try {
+    const ipAddress = req.header("x-forwarded-for") || req.socket.remoteAddress;
+    res.send(`
+      <center>
+        <h1>Welcome To Denonime API</h1>
+        <h4>Client From ${ipAddress}</h4>
+      <center>
+    `);
+  } catch (error) {
+    res.send("Terjadi Kegagalan pada server cek log...");
+  }
+});
+
+app.all("*", (req, res) => {
+  res.status(404);
+  res.json({
+    status: "fail",
+    message: "Url Not Found",
+  });
+});
+
 const port = 3000 || process.env.PORT;
 
 app.listen(port, () => {
