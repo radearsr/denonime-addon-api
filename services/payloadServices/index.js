a34ffd82925eb1ee503b520c29e630543a9f4121
radearsr <radea.surya1@gmail.com>
2023-02-21 21:21:18 +0700

    update adding payload services

---
+++ services/payloadServices/index.js
@@ -0,0 +1,23 @@
+exports.monitoringPayloadSchema = (payload) => {
+  if (!payload.link || !payload.lastTotalEps) {
+    throw new Error("Payload belum sesuai, pastikan 2 fields terisi yaitu link dan lastTotalEps");
+  }
+};
+
+exports.getEmbedPayloadSchema = (payload) => {
+  if (!payload.link) {
+    throw new Error("Payload belum sesuai, pastikan link fields sudah terisi");
+  }
+};
+
+exports.getSourcePayloadSchema = (payload) => {
+  if (!payload.link || !payload.strategy) {
+    throw new Error("Payload belum sesuai, pastikan link dan strategy('own', 'otakudesu') fields sudah terisi");
+  } 
+};
+
+exports.getSourceHeaderSchema = (headers) => {
+  if (!headers["sec-ch-ua"] || !headers["sec-ch-ua-mobile"] || !headers["sec-ch-ua-platform"] || !headers["user-agent"]) {
+    throw new Error("Header belum sesuai pastikan sec-ch-ua, sec-ch-ua-mobile, sec-ch-ua-platform, user-agent terisi");
+  }
+}
